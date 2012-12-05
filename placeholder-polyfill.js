d8a893d32cb6d937e28173bd4f2bfe32aa21ec98
James Thompson <james@ascensor.co.uk>
2012-12-05 14:56:18 +0000

    Initial Commit

---
+++ placeholder-polyfill.js
@@ -0,0 +1,26 @@
+
+if(!Modernizr.input.placeholder){
+	$('input, textarea').each(function(){                                                                       
+    	var self = $(this),
+            placeholderText = self.attr('placeholder'),
+        	label = self.parent().find($('label')); 
+
+    	if(placeholderText){
+        	label
+            	.show()
+            	.css({
+                	width: self.width(),
+                	height: self.height()
+            	});
+
+        	self.on('focus', function(){
+        	   label.hide();
+        	})
+        	.on('blur', function(){
+                if (self.attr('value') == '') {
+            	   label.show();
+            	}
+        	});
+    	}                                                                              
+	});
+}
